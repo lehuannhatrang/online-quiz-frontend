89a9de9945844ddca52094a7aa0fcdf7cfbc672e
Le Huan <lehuannhatrang98@gmail.com>
2019-04-15 11:10:24 +0700

    [MASTER] init and configurate project

---
+++ src/containers/dashboard/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class DashBoard extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <h1>Login Successfull</h1>
+        )
+    }
+
+}
+
+export default DashBoard;
